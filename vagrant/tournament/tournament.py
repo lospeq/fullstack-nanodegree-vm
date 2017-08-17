ae020eb07c8ab5b095644064609cef9519ace17b
Lospeq <iamsorekno@gmail.com>
2017-08-17 08:31:48 +0600

    complete Swiss Tournament project

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,6 +4,8 @@
 #
 
 import psycopg2
+from sys import maxsize
+from bleach import clean
 
 
 def connect():
@@ -13,25 +15,44 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("delete from Matches;")
+    conn.commit()
+    conn.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("delete from Players;")
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("select count(id) from Players;")
+    res = cur.fetchall()
+    conn.close()
+    return res[0][0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("insert into Players(name) values(%s)", (clean(name),))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +68,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("""select p.id, p.name, coalesce((select win_count from wins_v w where p.id = w.win_id),0) as wins, 
+                    coalesce((select match_count from matches_v m where p.id = m.id),0) as matches
+                    from Players p
+                    order by wins desc""")
+    res = cur.fetchall()
+    conn.close()
+    return res
 
 
 def reportMatch(winner, loser):
@@ -56,16 +86,20 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("insert into Matches(win_id, los_id) values(%s, %s)", (clean(winner), clean(loser)))
+    conn.commit()
+    conn.close()
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +107,9 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
+    stan = playerStandings()
+    res = []
+    for i in range(len(stan))[::2]:
+        res.append((stan[i][0], stan[i][1], stan[i+1][0], stan[i+1][1]))   
+    return res
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,26 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament;
 
+CREATE TABLE Players(
+id SERIAL PRIMARY KEY,
+name TEXT);
+
+CREATE TABLE Matches(
+	id SERIAL PRIMARY KEY,
+	win_id INTEGER REFERENCES Players(id),
+	los_id INTEGER REFERENCES Players(id));
+
+
+create view wins_v as 
+	select win_id, count(win_id) as win_count
+	from Matches
+	group by win_id;
+
+create view matches_v as
+	select p.id, count(m.id) as match_count
+	from Players p left join Matches m
+		on p.id = m.win_id or p.id = m.los_id
+		group by p.id;
